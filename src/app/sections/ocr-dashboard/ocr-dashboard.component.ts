e5029876947f8999c85681d7309be727af5e7ff6
Bettina Nicholson <bettinanicholson@unknown1ab06adb0f50.attlocal.net>
2026-08-10 12:01:18 -0500

    Checks for file size and file type.

--- src/app/sections/ocr-dashboard/ocr-dashboard.component.ts
+++ src/app/sections/ocr-dashboard/ocr-dashboard.component.ts
@@ -29,11 +29,26 @@ export class OcrDashboardComponent {
 
   public onFileUpload(event: any): void {
     const file = event.target.files[0];
+    const maxFileSize = 2 * 1024 * 1024; // 2MB TODO: split this out into an input
 
-    // if (!file) {
-    //   console.error('No file selected');
-    //   return;
-    // }
+    if (!file) {
+      console.error('No file selected');
+      return;
+    }
+
+    if (file.size > maxFileSize) {
+      console.error('File size exceeds the limit');
+      // TODO: show error message to user
+      return;
+    }
+
+    // TODO: adjust once not testing file type
+    if (file.type !== 'text/csv' && file.type !== 'video/mp4') {
+      console.error('Invalid file type');
+    }
+
+    // console.log('File uploaded:', file.size, maxFileSize);
+
 
     const reader = new FileReader();
     // let csvData: string | ArrayBuffer | null = null;
@@ -43,20 +58,24 @@ export class OcrDashboardComponent {
     // CSV data: 457500000,664371495,333333333,457508000,555555555,666666666,777777777,861100036,861100036,123456789
 
     reader.onload = () => {
-      // TODO: clear out old data to avoid duplicates when uploading a new file
-      this.policies = [];
-
-      csvData = reader.result;
-      let csvDataRow = csvData?.split(',');
+        // TODO: clear out old data to avoid duplicates when uploading a new file
+        this.policies = [];
 
 
-      csvDataRow?.forEach((policyNumber: string) => {
-        this.policies.push({ policyNumber });
-      });
+      // TODO: remove this if statement once done testing file size with other file types. This is just to test the file size limit with a csv file.
+      if (file.type === 'text/csv') {
 
-      console.log('CSV data:', csvData);
-      console.log('Policies:', this.policies);
+        csvData = reader.result;
+        let csvDataRow = csvData?.split(',');
 
+
+        csvDataRow?.forEach((policyNumber: string) => {
+          this.policies.push({ policyNumber });
+        });
+
+        console.log('CSV data:', csvData);
+        console.log('Policies:', this.policies);
+      }
     }
 
     reader.readAsText(file);
