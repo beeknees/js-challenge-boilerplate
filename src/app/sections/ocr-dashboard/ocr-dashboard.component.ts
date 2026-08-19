ae530435fef9d12c5e14948c8a863c3a47ff5a21
Bettina Nicholson <bettinanicholson@Bettinas-MBP-2.attlocal.net>
2026-08-19 10:34:12 -0500

    Adds checksum to check if policy is valid.

--- src/app/sections/ocr-dashboard/ocr-dashboard.component.ts
+++ src/app/sections/ocr-dashboard/ocr-dashboard.component.ts
@@ -15,6 +15,38 @@ export class OcrDashboardComponent {
 
   constructor(private cdr: ChangeDetectorRef) {}
 
+  checkPolicyValidity(policy: string): any {
+    console.log('policy', policy);
+
+    let policyDigits = Array.from(policy, Number).reverse();
+    let policyDigitsCollection: number[] = [];
+    
+    // console.log('POLICY DIGITS', policyDigits);
+
+    policyDigits.forEach((digit: number, index) => {
+      let multipliedDigit = digit * (index + 1);
+      // for (let i = 0; i < 9; i++) {
+        
+      //   console.log(i + 1, digit);
+        
+      // }
+      // console.log('multipliedDigit', multipliedDigit, 'RESULT', digit, index);
+      policyDigitsCollection.push(multipliedDigit);
+    });
+    // console.log('COLLECTIONS', policyDigitsCollection);
+
+    const result = policyDigitsCollection.reduce((acc, curr) => acc + curr, 0);
+
+    // const additionDisp = result;
+    // console.log('RESULT AFTER ADDING', result / 11);
+
+    const isDivisibleBy11 = (num: number) => num % 11 === 0;
+
+    // console.log('isDivisibleBy11', isDivisibleBy11(result));
+    return isDivisibleBy11(result);
+
+  }
+
   onSelectedFile(file: Event): void {
     const fileInput = file.target as HTMLInputElement;
     const maxFileSize = 2 * 1024 * 1024; // 2MB - can this be set in a config file or environment variable? Or is it better to hardcode it here?
@@ -43,7 +75,17 @@ export class OcrDashboardComponent {
 
         if (typeof result === 'string') {
           const resultItems = result?.split(',');
-          this.policies = resultItems.map((policyNumber) => ({ policyNumber }));
+          // this.policies = resultItems.map((policyNumber) => ({ policyNumber }));
+
+          resultItems.map((item) => {
+            console.log('item', item, {policyNumber: item, isValid: this.checkPolicyValidity(item)});
+            this.policies.push({
+              policyNumber: item,
+              isValid: this.checkPolicyValidity(item)
+            })
+          });
+
+          console.log('test', 'POLICIES', this.policies)
         }
 
         this.cdr.detectChanges(); // Manually trigger change detection to update the view and resolve issues with displaying old csv data.
